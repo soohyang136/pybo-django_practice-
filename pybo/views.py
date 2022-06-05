3adb7bb60213ab9f4e35ec9c1ff720e085630acf
전수향 <jeonsuhyang@jeonsuhyang-ui-MacBookPro.local>
2022-06-05 23:13:08 +0900

    bootstrap, question_form, navbar, paging 기능 추가

---
+++ pybo/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Question
+from django.utils import timezone
+from .forms import QuestionForm
+from django.core.paginator import Paginator
+
+def index(request) :
+    page = request.GET.get('page', '1')
+    question_list = Question.objects.order_by('-create_date')
+    paginator = Paginator(question_list, 10)
+    page_obj = paginator.get_page(page)
+    context = {'question_list': page_obj}
+    return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question' : question}
+    return render(request, 'pybo/question_detail.html', context)
+
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('pybo:detail', question_id=question.id)
+
+def question_create(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    context = {'form' : form}
+    return render(request, 'pybo/question_form.html', context)
+
